1b57fea13f1c7a5e2bd040b9f8905656e6d95a16
warezthief <aguleria1@gmail.com>
2017-02-12 23:03:13 +0530

    Calculated Total poulation of elements in array.

--- aggregateTree.js
+++ aggregateTree.js
@@ -1,3 +1,49 @@
 /**
  * Created by pg on 12/02/17.
  */
+
+
+var T  = [
+    ['Country','Sex','Martial Status','Population'],
+    ['India','M','Married',131313],
+    ['India','M','Single',31131],
+    ['Australia','F','Single',391326],
+    ['Russia','M','Married',263952],
+    ['Australia','F','Married',316213]
+]
+
+var A= ['Country','Sex','Martial Status',null];
+
+function aggregate(T,A){
+
+    if(Array.isArray(T)){
+
+        var headers = T.shift();
+
+        var arrayAsObject = T.map(function(values){
+
+            return headers.reduce(function(finalObj,line,i){
+
+                finalObj[line] = values[i];
+
+                return finalObj;
+            },{}); /// Initialize finalObj to empty object {}
+
+        });
+
+      var TotalPopulation =   arrayAsObject.reduce(function(sum,line,iteration){
+
+            sum =sum + line.Population;
+           return sum;
+        },0); /// Initial Value for param sum is 0
+
+       // console.log('arrayAsObject is ', JSON.stringify(arrayAsObject,null,2));
+        console.log('Total Population ', JSON.stringify(TotalPopulation));
+
+    }
+    else{
+        console.log('Input param isn\'t a array');
+    }
+}
+
+aggregate(T,A);
